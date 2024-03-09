6e9e6115b49261dcec2d11e73e1115ba7c430e0a
Alan Jui <AlanJui.1960@gmail.com>
2024-03-09 11:43:20 +0800

    強化字典的查詢功能。

--- p100_tsa_han_ji_thok_im.py
+++ p100_tsa_han_ji_thok_im.py
@@ -24,9 +24,31 @@ def close_database():
 
 # =========================================================
 # 查漢字讀音
-# 在【十五音字庫】資料表查找【注音碼】
 # =========================================================
 def tsa_han_ji_thok_im(han_ji):
+    global db_cursor
+    sql = (
+        "SELECT H.Han_Ji, H.TL_Phing_Im, H.freq, L.Siann, L.Un, L.Tiau "
+        "FROM Han_Ji_Phing_Im_Ji_Tian AS H "
+        "JOIN Lui_Tsip_Nga_Siok_Thong AS L ON H.NST_ID = L.ID "
+        f"WHERE H.Han_Ji='{han_ji}';"
+    )
+    db_cursor.execute(sql)
+    results = db_cursor.fetchall()
+
+    # 將查詢結果轉換為字典
+    result_dict = {}
+    for result in results:
+        if result[0] not in result_dict:
+            result_dict[result[0]] = []
+        result_dict[result[0]].append({"TL_Phing_Im": result[1], "freq": result[2], "Siann": result[3], "Un": result[4], "Tiau": result[5]})
+
+    return result_dict
+
+# =========================================================
+# 查詢十五音字典
+# =========================================================
+def Tsa_Sip_Ngoo_Im(han_ji):
     global db_cursor
     sql = (
         "SELECT id, han_ji, tl_im, freq, siann, un, tiau, siann_bu, un_bu "
@@ -37,14 +59,35 @@ def tsa_han_ji_thok_im(han_ji):
     db_cursor.execute(sql)
     return db_cursor.fetchall()
 
+# =========================================================
+# 查詢彙集雅俗通十五音字典
+# =========================================================
+def Tsa_Nga_Siok_Thong(han_ji):
+    global db_cursor
+    sql = (
+        "SELECT * "
+        f"FROM Lui_Tsip_Nga_Siok_Thong "
+        f"WHERE Ji='{han_ji}';"
+    )
+    db_cursor.execute(sql)
+    return db_cursor.fetchall()
+
+
 
 if __name__ == "__main__":
     # 在程式開始時設定資料庫
     setup_database()
 
     # 查詢【字庫】資料庫
-    result = tsa_han_ji_thok_im("鍵")
+    han_ji = "鍵"
+    result = tsa_han_ji_thok_im(han_ji)
     print(result)
+    
+    result2 = Tsa_Sip_Ngoo_Im(han_ji)
+    print(result2)
+
+    result3 = Tsa_Nga_Siok_Thong(han_ji)
+    print(result3)
 
     # 在程式結束時關閉資料庫
     close_database()
