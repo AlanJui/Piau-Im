7e9c31e6fbd1178224ba877f8d8d3df7972bcb44
Alan Jui <AlanJui.1960@gmail.com>
2024-05-03 16:34:55 +0800

    新增 Batch 作業工具。

--- a900_Batch_Process.py
+++ a900_Batch_Process.py
@@ -1,26 +1,32 @@
 # a900_Batch_Process.py a520_製作注音網頁.py -i output xlsx
 import argparse
-import glob
-import os
 import subprocess
 import sys
 
 # 解析命令行參數
 parser = argparse.ArgumentParser()
 parser.add_argument('executable', help='可在 command line 執行的執行檔名稱')
-parser.add_argument('-i', dest='input_directory', help='輸入目錄')
-parser.add_argument('-e', dest='extension', default='*', help='副檔名')
+parser.add_argument('-d', dest='directory', default='output', help='輸入目錄')
+parser.add_argument('-t', dest='todo_file_name', default='todo_list.txt', help='待處理清單檔案名稱')
 args = parser.parse_args()
 
-# 收集指定子目錄下的所有指定副檔名的文件
-file_paths = glob.glob(os.path.join(args.input_directory, f'*.{args.extension}'))
+# 從命令列參數取得 Python 程式的名稱
+python_program = args.executable
 
-# 過濾掉 exculude_list 中的文件
-# 要排除的文件名稱
-exculude_list = ['Piau-Tsu-Im.xlsx', 'env.xlsx', 'env_osX.xlsx']
-files = [os.path.basename(file_path) for file_path in file_paths if os.path.basename(file_path) not in exculude_list and not os.path.basename(file_path).startswith('~$')]
+# 獲取當前 Python 環境的路徑
+python_path = sys.executable
 
-# 啟動命令行執行檔並傳入文件列表
-for file in files:
-    filename = os.path.basename(file)
-    subprocess.run([sys.executable, args.executable, '-i', filename])
+# 取得使用者指定之 TODO List 檔案名稱
+todo_list = args.todo_file_name
+
+# 取得使用者指定之目錄路徑
+directory_path = args.directory
+
+# 開啟 TODO List 檔案並讀取每一行
+with open(todo_list, 'r', encoding='utf-8') as file:
+    for line in file:
+        # 移除換行符號
+        input_file = line.strip()
+
+        # 使用 subprocess 執行指定的 Python 程式，並將該行作為輸入檔案參數
+        subprocess.run([python_path, python_program, '-d', directory_path, '-i', input_file])
