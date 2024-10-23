af0c4cb6c16300faccb92ba47e33117ace4f0ff7
Alan Jui <alanjui.1960@gmail.com>
2024-10-23 22:45:10 +0800

    修訂 Kong_Un_V5

--- a300_漢字查標音.py
+++ a300_漢字查標音.py
@@ -5,10 +5,10 @@ import os
 import sys
 
 from mod_廣韻 import (
-    cha_ciat_gu_ha_ji,
-    cha_ciat_gu_siong_ji,
+    ca_siann_bu_piau_im,
+    ca_un_bu_piau_im,
     connect_to_db2,
-    han_ji_cha_piau_im,
+    han_ji_ca_piau_im,
 )
 
 
@@ -18,79 +18,76 @@ def main():
         print("請輸入欲查詢讀音之【漢字】(無需輸入切語之反切上字及下字)!")
         sys.exit(-1)
 
+    # 取得使用者之輸入：欲查詢讀音的漢字
     beh_cha_e_han_ji = sys.argv[1]
 
     # 連上 DB
-    with connect_to_db2('.\\Kong_Un_V2.db') as conn:
+    with connect_to_db2('.\\Kong_Un.db') as conn:
         cursor = conn.cursor()
 
         # 查漢字之切語
-        han_ji_piau_im = han_ji_cha_piau_im(cursor, beh_cha_e_han_ji)
+        han_ji_piau_im = han_ji_ca_piau_im(cursor, beh_cha_e_han_ji)
         if not han_ji_piau_im:
             print("找不到此【漢字】!")
             sys.exit(-1)
         
-        ciat_gu = han_ji_piau_im[0]['切語']    
-        siong_ji = ciat_gu[0]
-        ha_ji = ciat_gu[1]
+        上字 = han_ji_piau_im[0]['上字']    
+        下字 = han_ji_piau_im[0]['下字']    
 
         # 顯示結果
         os.system('cls')
         print('\n=================================================')
         print(f'查詢漢字：【{beh_cha_e_han_ji}】')
-        print(f'切語：{ciat_gu}')
-        print(f'標音：{han_ji_piau_im[0]["漢字標音"]}')
-        print(f'字義：{han_ji_piau_im[0]["字義"]}')
-
-        # 顯示廣韻參考資料
-        print('\n-------------------------------------------------')
-        print('【廣韻查找資料】：\n')
-        print(f'目次：{han_ji_piau_im[0]["小韻目次"]}，小韻字序號：{han_ji_piau_im[0]["小韻字序號"]}，小韻字：{han_ji_piau_im[0]["小韻字"]}')
+        print(f'切語：{上字}{下字}切')
+        print(f'標音：{han_ji_piau_im[0]["標音"]}')
+        # print(f'字義：{han_ji_piau_im[0]["字義"]}')
 
         # 查詢反切上字
         print('\n-------------------------------------------------')
         print('【切語上字】：\n')
-        siong_ji_im = han_ji_cha_piau_im(cursor, siong_ji)
-        siong_ji_piau = cha_ciat_gu_siong_ji(cursor, siong_ji)
-        if not siong_ji_piau:
-            print(f'查不到【反切上字】：{siong_ji}')
-        else:
-            print(f"切語上字 = {siong_ji} (標音：{siong_ji_im[0]['漢字標音']})")
-            print(f"聲母：{siong_ji_piau[0]['聲母']} [{siong_ji_piau[0]['聲母拼音碼']}]，國際音標：/{siong_ji_im[0]['聲母國際音標']}/ ")
-            print(f"(發音部位：{siong_ji_piau[0]['發音部位']}，清濁：{siong_ji_piau[0]['清濁']}，發送收：{siong_ji_piau[0]['發送收']})")
+        聲母 = han_ji_piau_im[0]['聲母']
+        聲母標音 = han_ji_piau_im[0]['聲母標音']
+        七聲類 = han_ji_piau_im[0]['七聲類']
+        清濁 = han_ji_piau_im[0]['清濁']
+        發送收 = han_ji_piau_im[0]['發送收']
+        聲母其它標音 = ca_siann_bu_piau_im(cursor, 聲母標音)
+        聲母國際音標 = 聲母其它標音[0]['國際音標聲母']
+        聲母方音符號 = 聲母其它標音[0]['方音聲母']
+        print(f"切語上字 = {上字}")
+        print(f"聲母：{聲母} [{聲母標音}]，國際音標：/{聲母國際音標}/，方音符號：{聲母方音符號}")
+        print(f"(發音部位：{七聲類}，清濁：{清濁}，發送收：{發送收})")
 
         # 查詢反切下字
         print('\n-------------------------------------------------')
         print('【切語下字】：\n')
-        ha_ji_im = han_ji_cha_piau_im(cursor, ha_ji)
-        ha_ji_piau = cha_ciat_gu_ha_ji(cursor, ha_ji)
-        if not ha_ji_piau:
-            print(f'查不到【反切下字】：{ha_ji}')
-        else:
-            print(f"切語下字 = {ha_ji} (標音：{ha_ji_im[0]['漢字標音']})")
-            print(f"韻母：{ha_ji_piau[0]['韻母']} [{ha_ji_piau[0]['韻母拼音碼']}]，國際音標：/{ha_ji_im[0]['韻母國際音標']}/")
-            print(f"攝：{ha_ji_piau[0]['攝']}，調：{ha_ji_piau[0]['調']}聲，目次：{ha_ji_piau[0]['目次']}")
-            print(f"{ha_ji_piau[0]['韻系']}韻系，{ha_ji_piau[0]['韻目']}韻，{ha_ji_piau[0]['呼']}口呼，{ha_ji_piau[0]['等']}等 ({ha_ji_piau[0]['等呼']})")
+        韻母 = han_ji_piau_im[0]['韻母']
+        韻母標音 = han_ji_piau_im[0]['韻母標音']
+        攝 = han_ji_piau_im[0]['攝']
+        調 = han_ji_piau_im[0]['調']
+        目次 = han_ji_piau_im[0]['目次']
+        韻目 = han_ji_piau_im[0]['韻目']
+        等 = han_ji_piau_im[0]['等']
+        呼 = han_ji_piau_im[0]['呼']
+        等呼 = han_ji_piau_im[0]['等呼']
+        韻母其它標音 = ca_un_bu_piau_im(cursor, 韻母標音)
+        韻母國際音標 = 韻母其它標音[0]['國際音標韻母']
+        韻母方音符號 = 韻母其它標音[0]['方音韻母']
+        print(f"切語下字 = {下字}") 
+        print(f"韻母：{韻母} [{韻母標音}]，國際音標：/{韻母國際音標}/，方音符號：{韻母方音符號}")
+        print(f"攝：{攝}，調：{調}聲，目次：{目次}")
+        print(f"韻目：{韻目}，{等}等，{呼}口 ({等呼})")
 
         # 組合拼音
-        tiau_ho_list = {
-            '清平': 1,
-            '清上': 2,
-            '清去': 3,
-            '清入': 4,
-            '濁平': 5,
-            '濁上': 2,
-            '濁去': 7,
-            '濁入': 8,
-        }
-        siann = siong_ji_piau[0]['聲母拼音碼']
-        cing_tok_str = siong_ji_piau[0]['清濁']
-        cing_tok = cing_tok_str[-1]
-        un = ha_ji_piau[0]['韻母拼音碼']
-        tiau_ho = tiau_ho_list[ f"{cing_tok}{ha_ji_piau[0]['調']}" ]
-
+        廣韻調名 = han_ji_piau_im[0]['廣韻調名']
+        台羅聲調 = han_ji_piau_im[0]['台羅聲調']
         print('\n-------------------------------------------------')
-        print(f'【切語拼音】：{ciat_gu} [{siann}{un}{tiau_ho}]\n')
+        print('【聲調】：\n')
+        print(f'上字取聲之【清濁】，下字取調之【陰陽】，得：{廣韻調名}調；')
+        print(f'推導【台羅聲調】得：第 {台羅聲調} 調。')
+        print('\n-------------------------------------------------')
+        print(f'【切語拼音】：\n')
+        print(f'台語音標：{聲母標音}{韻母標音}{台羅聲調}')
+        print(f'方音符號：{聲母方音符號}{韻母方音符號}{台羅聲調}\n')
 
 
 if __name__ == "__main__":
