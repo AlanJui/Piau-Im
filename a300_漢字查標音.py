7c678bc025aa0dcdd2a1b2a46460530576c14fd4
Alan Jui <alanjui.1960@gmail.com>
2024-10-23 23:08:37 +0800

    製作 "廣韻V5" 版

--- a300_漢字查標音.py
+++ a300_漢字查標音.py
@@ -2,6 +2,7 @@
 # 輸入漢字，查詢廣韻反切標音
 #==============================================================================
 import os
+import sqlite3
 import sys
 
 from mod_廣韻 import (
@@ -11,8 +12,7 @@ from mod_廣韻 import (
     han_ji_ca_piau_im,
 )
 
-
-def main():
+if __name__ == "__main__":
     # 確認使用者有輸入反切之切語參數
     if len(sys.argv) != 2:
         print("請輸入欲查詢讀音之【漢字】(無需輸入切語之反切上字及下字)!")
@@ -21,35 +21,32 @@ def main():
     # 取得使用者之輸入：欲查詢讀音的漢字
     beh_cha_e_han_ji = sys.argv[1]
 
-    # 連上 DB
-    with connect_to_db2('.\\Kong_Un.db') as conn:
-        cursor = conn.cursor()
+    # 建立資料庫連線
+    connection = sqlite3.connect('Kong_Un.db')
+    cursor = connection.cursor()
+    
+    han_ji_piau_im = han_ji_ca_piau_im(cursor, beh_cha_e_han_ji)
 
-        # 查漢字之切語
-        han_ji_piau_im = han_ji_ca_piau_im(cursor, beh_cha_e_han_ji)
-        if not han_ji_piau_im:
-            print("找不到此【漢字】!")
-            sys.exit(-1)
-        
-        上字 = han_ji_piau_im[0]['上字']    
-        下字 = han_ji_piau_im[0]['下字']    
+    os.system('cls')
+    for record in han_ji_piau_im:
+        上字 = record['上字']    
+        下字 = record['下字']    
 
         # 顯示結果
-        os.system('cls')
         print('\n=================================================')
-        print(f'查詢漢字：【{beh_cha_e_han_ji}】')
+        print(f'查詢漢字：【{record["漢字"]}】(字號：{record["字號"]})')
         print(f'切語：{上字}{下字}切')
-        print(f'標音：{han_ji_piau_im[0]["標音"]}')
-        # print(f'字義：{han_ji_piau_im[0]["字義"]}')
+        print(f'標音：{record["標音"]}')
+        # print(f'字義：{record["字義"]}')
 
         # 查詢反切上字
         print('\n-------------------------------------------------')
         print('【切語上字】：\n')
-        聲母 = han_ji_piau_im[0]['聲母']
-        聲母標音 = han_ji_piau_im[0]['聲母標音']
-        七聲類 = han_ji_piau_im[0]['七聲類']
-        清濁 = han_ji_piau_im[0]['清濁']
-        發送收 = han_ji_piau_im[0]['發送收']
+        聲母 = record['聲母']
+        聲母標音 = record['聲母標音']
+        七聲類 = record['七聲類']
+        清濁 = record['清濁']
+        發送收 = record['發送收']
         聲母其它標音 = ca_siann_bu_piau_im(cursor, 聲母標音)
         聲母國際音標 = 聲母其它標音[0]['國際音標聲母']
         聲母方音符號 = 聲母其它標音[0]['方音聲母']
@@ -60,15 +57,15 @@ def main():
         # 查詢反切下字
         print('\n-------------------------------------------------')
         print('【切語下字】：\n')
-        韻母 = han_ji_piau_im[0]['韻母']
-        韻母標音 = han_ji_piau_im[0]['韻母標音']
-        攝 = han_ji_piau_im[0]['攝']
-        調 = han_ji_piau_im[0]['調']
-        目次 = han_ji_piau_im[0]['目次']
-        韻目 = han_ji_piau_im[0]['韻目']
-        等 = han_ji_piau_im[0]['等']
-        呼 = han_ji_piau_im[0]['呼']
-        等呼 = han_ji_piau_im[0]['等呼']
+        韻母 = record['韻母']
+        韻母標音 = record['韻母標音']
+        攝 = record['攝']
+        調 = record['調']
+        目次 = record['目次']
+        韻目 = record['韻目']
+        等 = record['等']
+        呼 = record['呼']
+        等呼 = record['等呼']
         韻母其它標音 = ca_un_bu_piau_im(cursor, 韻母標音)
         韻母國際音標 = 韻母其它標音[0]['國際音標韻母']
         韻母方音符號 = 韻母其它標音[0]['方音韻母']
@@ -78,8 +75,8 @@ def main():
         print(f"韻目：{韻目}，{等}等，{呼}口 ({等呼})")
 
         # 組合拼音
-        廣韻調名 = han_ji_piau_im[0]['廣韻調名']
-        台羅聲調 = han_ji_piau_im[0]['台羅聲調']
+        廣韻調名 = record['廣韻調名']
+        台羅聲調 = record['台羅聲調']
         print('\n-------------------------------------------------')
         print('【聲調】：\n')
         print(f'上字取聲之【清濁】，下字取調之【陰陽】，得：{廣韻調名}調；')
@@ -89,6 +86,5 @@ def main():
         print(f'台語音標：{聲母標音}{韻母標音}{台羅聲調}')
         print(f'方音符號：{聲母方音符號}{韻母方音符號}{台羅聲調}\n')
 
-
-if __name__ == "__main__":
-    main()
+    # 關閉資料庫連線
+    connection.close()
--- a400_反切查拼音.py
+++ a400_反切查拼音.py
@@ -2,17 +2,12 @@
 # 程式用於查詢反切拼音
 #==============================================================================
 import os
+import sqlite3
 import sys
 
-from mod_廣韻 import (
-    ca_siann_bu_piau_im,
-    ca_un_bu_piau_im,
-    connect_to_db2,
-    huan_ciat_ca_piau_im,
-)
+from mod_廣韻 import ca_siann_bu_piau_im, ca_un_bu_piau_im, huan_ciat_ca_piau_im
 
-
-def main():
+if __name__ == "__main__":
     # 確認使用者有輸入反切之切語參數
     if len(sys.argv) != 2:
         print("請輸入欲查詢讀音之【切語】(反切上字及下字)!")
@@ -25,40 +20,34 @@ def main():
         print("反切用的切語，必須有兩個漢字！")
         sys.exit(-1)
 
-    # 連上 DB
-    with connect_to_db2('.\\Kong_Un.db') as conn:
-        cursor = conn.cursor()
+    siong_ji, ha_ji = ciat_gu[0], ciat_gu[1]
 
-        # 根據反切上字和反切下字來查詢台羅拼音
-        siong_ji = ciat_gu[0]
-        ha_ji = ciat_gu[1]
-
-        # 透過切語之上字與下字，反切出漢字標音
-        han_ji_piau_im = huan_ciat_ca_piau_im(cursor, siong_ji, ha_ji)
-        if not han_ji_piau_im:
-            print("找不到此【漢字】!")
-            sys.exit(-1)
-        
-        漢字 = han_ji_piau_im[0]['漢字']    
-        上字 = han_ji_piau_im[0]['上字']    
-        下字 = han_ji_piau_im[0]['下字']    
+    # 建立資料庫連線
+    connection = sqlite3.connect('Kong_Un.db')
+    cursor = connection.cursor()
+    
+    han_ji_piau_im = huan_ciat_ca_piau_im(cursor, siong_ji, ha_ji)
+    
+    os.system('cls')
+    for record in han_ji_piau_im:
+        上字 = record['上字']    
+        下字 = record['下字']    
 
         # 顯示結果
-        os.system('cls')
         print('\n=================================================')
-        print(f'查詢漢字：【{漢字}】')
+        print(f'查詢漢字：【{record["漢字"]}】(字號：{record["字號"]})')
         print(f'切語：{上字}{下字}切')
-        print(f'標音：{han_ji_piau_im[0]["標音"]}')
-        # print(f'字義：{han_ji_piau_im[0]["字義"]}')
+        print(f'標音：{record["標音"]}')
+        # print(f'字義：{record["字義"]}')
 
         # 查詢反切上字
         print('\n-------------------------------------------------')
         print('【切語上字】：\n')
-        聲母 = han_ji_piau_im[0]['聲母']
-        聲母標音 = han_ji_piau_im[0]['聲母標音']
-        七聲類 = han_ji_piau_im[0]['七聲類']
-        清濁 = han_ji_piau_im[0]['清濁']
-        發送收 = han_ji_piau_im[0]['發送收']
+        聲母 = record['聲母']
+        聲母標音 = record['聲母標音']
+        七聲類 = record['七聲類']
+        清濁 = record['清濁']
+        發送收 = record['發送收']
         聲母其它標音 = ca_siann_bu_piau_im(cursor, 聲母標音)
         聲母國際音標 = 聲母其它標音[0]['國際音標聲母']
         聲母方音符號 = 聲母其它標音[0]['方音聲母']
@@ -69,15 +58,15 @@ def main():
         # 查詢反切下字
         print('\n-------------------------------------------------')
         print('【切語下字】：\n')
-        韻母 = han_ji_piau_im[0]['韻母']
-        韻母標音 = han_ji_piau_im[0]['韻母標音']
-        攝 = han_ji_piau_im[0]['攝']
-        調 = han_ji_piau_im[0]['調']
-        目次 = han_ji_piau_im[0]['目次']
-        韻目 = han_ji_piau_im[0]['韻目']
-        等 = han_ji_piau_im[0]['等']
-        呼 = han_ji_piau_im[0]['呼']
-        等呼 = han_ji_piau_im[0]['等呼']
+        韻母 = record['韻母']
+        韻母標音 = record['韻母標音']
+        攝 = record['攝']
+        調 = record['調']
+        目次 = record['目次']
+        韻目 = record['韻目']
+        等 = record['等']
+        呼 = record['呼']
+        等呼 = record['等呼']
         韻母其它標音 = ca_un_bu_piau_im(cursor, 韻母標音)
         韻母國際音標 = 韻母其它標音[0]['國際音標韻母']
         韻母方音符號 = 韻母其它標音[0]['方音韻母']
@@ -87,8 +76,8 @@ def main():
         print(f"韻目：{韻目}，{等}等，{呼}口 ({等呼})")
 
         # 組合拼音
-        廣韻調名 = han_ji_piau_im[0]['廣韻調名']
-        台羅聲調 = han_ji_piau_im[0]['台羅聲調']
+        廣韻調名 = record['廣韻調名']
+        台羅聲調 = record['台羅聲調']
         print('\n-------------------------------------------------')
         print('【聲調】：\n')
         print(f'上字取聲之【清濁】，下字取調之【陰陽】，得：{廣韻調名}調；')
@@ -98,6 +87,5 @@ def main():
         print(f'台語音標：{聲母標音}{韻母標音}{台羅聲調}')
         print(f'方音符號：{聲母方音符號}{韻母方音符號}{台羅聲調}\n')
 
-
-if __name__ == "__main__":
-    main()
+    # 關閉資料庫連線
+    connection.close()
