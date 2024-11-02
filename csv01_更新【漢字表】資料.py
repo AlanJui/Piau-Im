c76acef9e6dd06e5cf5563a23a8e5a2d93141877
Alan Jui <alanjui.1960@gmail.com>
2024-11-02 22:27:10 +0800

    依文章【正氣歌】修訂資料庫。同時令【漢字表】資料錄可記載【建立時間】、【更新時間】。

--- csv01_更新【漢字表】資料.py
+++ csv01_更新【漢字表】資料.py
@@ -3,7 +3,7 @@ import sqlite3
 import pandas as pd
 
 # 設定 CSV 文件的檔案名稱
-csv_file_name = '廣韻v6_20241101-01_漢字表.csv'
+csv_file_name = 'Documents\\廣韻\\廣韻v6_20241101-01_漢字表.csv'
 
 # 使用 pandas 讀取 CSV 文件
 data = pd.read_csv(csv_file_name)
@@ -18,11 +18,15 @@ cursor = conn.cursor()
 # 使用 data 迭代每一行，並更新漢字表
 for index, row in data.iterrows():
     漢字號 = row['漢字號']
+    上字號 = row['上字號']
     下字號 = row['下字號']
+    標音 = row['標音']
 
     # 準備並執行更新語句
-    cursor.execute("UPDATE 漢字表 SET 下字號 = ? WHERE 漢字號 = ?", (下字號, 漢字號))
-
+    cursor.execute(
+        "UPDATE 漢字表 SET 字義號 = ?, 上字號 = ?, 下字號 = ?, 標音 = ? WHERE 漢字號 = ?",
+        (漢字號, 上字號, 下字號, 標音, 漢字號)
+    )
 # 提交更改並關閉連接
 conn.commit()
 conn.close()
