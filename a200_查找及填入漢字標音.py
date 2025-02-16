35a79ff1fc72f7076a034889fc865c7ec1a58fc3
居正中 <AlanJui.1960@gmail.com>
2025-02-16 13:29:11 +0800

    變更 a200 ：不再於此作業，建立【人工標音字庫】工作表

--- a200_查找及填入漢字標音.py
+++ a200_查找及填入漢字標音.py
@@ -114,23 +114,23 @@ def ca_han_ji_thak_im(wb, sheet_name='漢字注音', cell='V3', ue_im_lui_piat="
     piau_im_huat = get_value_by_name(wb=wb, name='標音方法')    # 指定【台語音標】轉換成【漢字標音】的方法
 
     # 建置自動及人工漢字標音字庫工作表：（1）【標音字庫】；（2）【人工標音字】；（3）【缺字表】
+    khuat_ji_piau_name = '缺字表'
+    # delete_sheet_by_name(wb=wb, sheet_name=khuat_ji_piau_name)
+    khuat_ji_piau_ji_khoo = JiKhooDict.create_ji_khoo_dict_from_sheet(
+                                wb=wb,
+                                sheet_name=khuat_ji_piau_name)
+
     piau_im_sheet_name = '標音字庫'
     # delete_sheet_by_name(wb=wb, sheet_name=piau_im_sheet_name)
     piau_im_ji_khoo = JiKhooDict.create_ji_khoo_dict_from_sheet(
                             wb=wb,
                             sheet_name=piau_im_sheet_name)
 
-    jin_kang_piau_im_sheet_name='人工標音字庫'
-    # delete_sheet_by_name(wb=wb, sheet_name=jin_kang_piau_im_sheet_name)
-    jin_kang_piau_im_ji_khoo = JiKhooDict.create_ji_khoo_dict_from_sheet(
-                                    wb=wb,
-                                    sheet_name=jin_kang_piau_im_sheet_name)
-
-    khuat_ji_piau_name = '缺字表'
-    # delete_sheet_by_name(wb=wb, sheet_name=khuat_ji_piau_name)
-    khuat_ji_piau_ji_khoo = JiKhooDict.create_ji_khoo_dict_from_sheet(
-                                wb=wb,
-                                sheet_name=khuat_ji_piau_name)
+    # jin_kang_piau_im_sheet_name='人工標音字庫'
+    # # delete_sheet_by_name(wb=wb, sheet_name=jin_kang_piau_im_sheet_name)
+    # jin_kang_piau_im_ji_khoo = JiKhooDict.create_ji_khoo_dict_from_sheet(
+    #                                 wb=wb,
+    #                                 sheet_name=jin_kang_piau_im_sheet_name)
 
     # 指定【漢字注音】工作表為【作用工作表】
     sheet = wb.sheets[sheet_name]
@@ -231,9 +231,9 @@ def ca_han_ji_thak_im(wb, sheet_name='漢字注音', cell='V3', ue_im_lui_piat="
         # 若已到【結尾】或【超過總行數】，則跳出迴圈
         if EOF or line > TOTAL_LINES:
             # 將【標音字庫】、【人工標音字庫】、【缺字表】三個字典，寫入 Excel 工作表
-            piau_im_ji_khoo.write_to_excel_sheet(wb=wb, sheet_name=piau_im_sheet_name)
-            jin_kang_piau_im_ji_khoo.write_to_excel_sheet(wb=wb, sheet_name=jin_kang_piau_im_sheet_name)
             khuat_ji_piau_ji_khoo.write_to_excel_sheet(wb=wb, sheet_name=khuat_ji_piau_name)
+            piau_im_ji_khoo.write_to_excel_sheet(wb=wb, sheet_name=piau_im_sheet_name)
+            # jin_kang_piau_im_ji_khoo.write_to_excel_sheet(wb=wb, sheet_name=jin_kang_piau_im_sheet_name)
             break
 
     #----------------------------------------------------------------------
