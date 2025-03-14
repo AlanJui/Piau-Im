53fa00e97d15ed114f2dd9fc154f45f9416edb78
居正中 <AlanJui.1960@gmail.com>
2025-03-14 21:58:32 +0800

    新增 a110 可用已標注 TLPA+ 音標之純文字檔，為【漢字注音】工作表填入漢字及標音。

--- a110_使用已標音文字檔填入.py
+++ a110_使用已標音文字檔填入.py
@@ -2,6 +2,7 @@
 # 載入程式所需套件/模組/函式庫
 # =========================================================================
 import logging
+import sys
 import unicodedata
 
 import xlwings as xw
@@ -9,7 +10,7 @@ import xlwings as xw
 # =========================================================================
 # 設定標點符號過濾
 # =========================================================================
-PUNCTUATIONS = (",", ".", "?", "!")
+PUNCTUATIONS = (",", ".", "?", "!", ":")
 
 # =========================================================================
 # 程式區域函式
@@ -31,10 +32,16 @@ def is_hanzi(char):
     return 'CJK UNIFIED IDEOGRAPH' in unicodedata.name(char, '')
 
 # =========================================================================
-# 用途：移除標點符號
+# 用途：移除標點符號並轉換TLPA+拼音格式
 # =========================================================================
 def clean_tlpa(word):
-    return ''.join(ch for ch in word if ch not in PUNCTUATIONS)
+    word = ''.join(ch for ch in word if ch not in PUNCTUATIONS)  # 移除標點符號
+    word = word.replace("oa", "ua")  # TLPA+ 調整，將 "oa" 變為 "ua"
+    if word.startswith("chh"):
+        word = "c" + word[3:]
+    elif word.startswith("ch"):
+        word = "z" + word[2:]
+    return word
 
 # =========================================================================
 # 用途：將漢字及TLPA標音填入Excel指定工作表
@@ -66,7 +73,7 @@ def fill_hanzi_and_tlpa(wb, filename='tmp.txt', sheet_name='漢字注音', start
             if cell_char and is_hanzi(cell_char):
                 sheet.cells(row_tlpa, col).value = tlpa_words[word_idx]
                 word_idx += 1
-                logging.info(f"已完成填入: {cell_char} - {tlpa_words[word_idx-1]}")
+                print(f"（{row_tlpa}, {col}）已填入: {cell_char} - {tlpa_words[word_idx-1]}")
             col += 1
 
     logging.info(f"已將漢字及TLPA注音填入【{sheet_name}】工作表！")
@@ -75,6 +82,9 @@ def fill_hanzi_and_tlpa(wb, filename='tmp.txt', sheet_name='漢字注音', start
 # 主作業程序
 # =========================================================================
 def main():
+    # 檢查是否有指定檔案名稱，若無則使用預設檔名
+    filename = sys.argv[1] if len(sys.argv) > 1 else "tmp.txt"
+    # 以作用中的Excel活頁簿為作業標的
     wb = xw.apps.active.books.active
     if wb is None:
         logging.error("無法找到作用中的Excel活頁簿。")
