b2311f959e38462455321db8980530e6341adb87
居正中 <AlanJui.1960@gmail.com>
2025-12-10 14:17:29 +0800

    重整

--- a110_使用漢字文字檔.py
+++ a110_使用漢字文字檔.py
@@ -581,10 +581,3 @@ def main():
 if __name__ == "__main__":
     exit_code = main()
     sys.exit(exit_code)
-if __name__ == "__main__":
-    exit_code = main()
-    sys.exit(exit_code)
-    exit_code = main()
-    sys.exit(exit_code)
-    exit_code = main()
-    sys.exit(exit_code)
